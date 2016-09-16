2b919c4194f2e7bed696c4cfb2f16259e40c736a
Andrzej Krzemieński <akrzemi1@gmail.com>
2016-09-16 14:34:08 +0200

    Update test_markable.cpp

--- test_markable.cpp
+++ test_markable.cpp
@@ -4,7 +4,7 @@
 // License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#include "markable.hpp"
+#include "include/markable.hpp"
 #include <cassert>
 #include <utility>
 #include <string>
