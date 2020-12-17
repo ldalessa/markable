e65183ab62faee8909b92c7b8ebff764218ef8ca
Andrzej Krzemienski <akrzemi1@gmail.com>
2020-12-17 23:09:11 +0100

    Add assignment functions

--- test/test_markable.cpp
+++ test/test_markable.cpp
@@ -29,14 +29,14 @@ void test_value_ctor()
     typedef markable< mark_int<int, -1> > opt_int;
     static_assert (sizeof(opt_int) == sizeof(int), "size waste");
     static_assert (!std::is_convertible<int, opt_int>(), "markable<T> must not be convertible from T");
-    
+
     opt_int oi_, oiN1(-1), oi0(0), oi1(1);
-	assert (oi_.storage_value() == -1);
+	  assert (oi_.storage_value() == -1);
     assert (!oi_.has_value());
     assert (!oiN1.has_value());
     assert ( oi0.has_value());
     assert ( oi1.has_value());
-    
+
     assert (oi0.value() == 0);
     assert (oi1.value() == 1);
   }
@@ -50,11 +50,12 @@ void test_value_ctor()
     assert ( oiN1.has_value());
     assert (!oi0.has_value());
     assert ( oi1.has_value());
-    
+
     assert (oiN1.value() == -1);
     assert (oi1.value() == 1);
   }
 }
+
 
 struct string_marked_value : markable_type<std::string>
 {
@@ -67,7 +68,7 @@ void test_string_traits()
 {
   typedef markable<string_marked_value> opt_str;
   static_assert (sizeof(opt_str) == sizeof(std::string), "size waste");
-  
+
   opt_str os_, os00(std::string("\0\0", 2)), os0(std::string("\0")), osA(std::string("A"));
   assert (!os_.has_value());
   assert (!os00.has_value());
@@ -79,7 +80,7 @@ struct mark_first_empty : markable_type< std::string, std::pair<bool, std::strin
 {
   static storage_type marked_value() { return storage_type(false, "anything"); }
   static bool is_marked_value(const storage_type& v) { return v.first == false; }
-  
+
   static const value_type& access_value(const storage_type& v) { return v.second; }
   static storage_type store_value(const value_type& v) { return storage_type(true, v); }
   static storage_type store_value(value_type&& v) { return storage_type(true, std::move(v)); }
@@ -89,17 +90,17 @@ void test_custom_storage()
 {
   typedef markable<mark_first_empty> opt_str;
   opt_str os_, os0(std::string("\0")), osA(std::string("A"));
-  
+
   assert (!os_.has_value());
-  
+
   assert (os0.has_value());
   assert (os0.value() == "");
-  
+
   assert (osA.has_value());
   assert (osA.value() == "A");
 
   swap (os_, os0);
-    
+
   assert (os_.has_value());
   assert (os_.value() == "");
   assert (!os0.has_value());
@@ -109,13 +110,13 @@ void test_bool_storage()
 {
   typedef markable<mark_bool> opt_bool;
   static_assert (sizeof(opt_bool) == 1, "size waste");
-  
+
   opt_bool ob_, obT(true), obF(false);
   assert (!ob_.has_value());
-  
+
   assert (obT.has_value());
   assert (obT.value() == true);
-  
+
   assert (obF.has_value());
   assert (obF.value() == false);
 }
@@ -124,7 +125,7 @@ void test_storage_value()
 {
   typedef markable< mark_int<int, -1> > opt_int;
   opt_int oi_, oiN1(-1), oi0(0), oi1(1);
-  
+
   assert ( oi_.storage_value() == -1);
   assert (oiN1.storage_value() == -1);
   assert ( oi0.storage_value() ==  0);
@@ -138,16 +139,16 @@ void test_mark_fp_nan()
   assert (!o_.has_value());
   assert ( o1.has_value());
   assert (!oNan.has_value());
-  
+
   assert (o1.value() == 1.0);
-  
+
   double v = o_.storage_value();
   assert (v != v);
-  
+
   v = o1.storage_value();
   assert (v == 1.0);
   assert (v == v);
-  
+
   v = oNan.storage_value();
   assert (v != v);
 }
@@ -157,13 +158,13 @@ void test_mark_value_init()
   {
     typedef markable<mark_value_init<int>> opt_t;
     opt_t o_, o1 (1), oE(0);
-    
+
     assert (!o_.has_value());
     assert ( o1.has_value());
     assert (!oE.has_value());
-    
+
     assert (o1.value() == 1);
-    
+
     assert (o_.storage_value() == 0);
     assert (o1.storage_value() == 1);
     assert (oE.storage_value() == 0);
@@ -171,13 +172,13 @@ void test_mark_value_init()
   {
     typedef markable<mark_value_init<std::string>> opt_t;
     opt_t o_, o1 (std::string("one")), oE ((std::string()));
-    
+
     assert (!o_.has_value());
     assert ( o1.has_value());
     assert (!oE.has_value());
-    
+
     assert (o1.value() == "one");
-    
+
     assert (o_.storage_value() == "");
     assert (o1.storage_value() == "one");
     assert (oE.storage_value() == "");
@@ -209,13 +210,13 @@ void test_mark_stl_empty()
   {
     typedef markable<mark_stl_empty<Cont>> opt_t;
     opt_t o_, o1 (Cont(false)), oE(Cont(true));
-    
+
     assert (objects_created_with_value == 3);
     assert (!o_.has_value());
     assert ( o1.has_value());
     assert (!oE.has_value());
     assert (objects_created_with_value == 3);
-       
+
     assert (o_.storage_value() == Cont(true));
     assert (o1.storage_value() == Cont(false));
     assert (oE.storage_value() == Cont(true));
@@ -223,13 +224,13 @@ void test_mark_stl_empty()
   {
     typedef markable<mark_stl_empty<std::string>> opt_t;
     opt_t o_, o1 (std::string("one")), oE ((std::string()));
-    
+
     assert (!o_.has_value());
     assert ( o1.has_value());
     assert (!oE.has_value());
-    
+
     assert (o1.value() == "one");
-    
+
     assert (o_.storage_value() == "");
     assert (o1.storage_value() == "one");
     assert (oE.storage_value() == "");
@@ -243,17 +244,36 @@ void test_mark_enum()
 {
   typedef markable<mark_enum<Dir, -1>> opt_dir;
   opt_dir o_, oN(Dir::N), oW(Dir::W);
-  
+
   assert (!o_.has_value());
   assert ( oN.has_value());
   assert ( oW.has_value());
-  
+
   assert (oN.value() == Dir::N);
   assert (oW.value() == Dir::W);
-  
+
   assert (o_.storage_value() == -1);
   assert (oN.storage_value() ==  0);
   assert (oW.storage_value() ==  3);
+}
+
+
+void test_assignment()
+{
+  typedef markable<mark_enum<Dir, -1>> opt_dir;
+  opt_dir od;
+  assert(!od.has_value());
+
+  od.assign(Dir::W);
+  assert(od.has_value());
+  assert(od.value() == Dir::W);
+
+  od.assign_storage(-1);
+  assert(!od.has_value());
+
+  od.assign_storage(0);
+  assert(od.has_value());
+  assert(od.value() == Dir::N);
 }
 
 
@@ -263,10 +283,10 @@ void test_optional_as_storage()
   typedef markable<mark_optional<boost::optional<int>>> opt_int;
   opt_int oi_, oiN1(-1), oi0(0);
   assert (!oi_.has_value());
-  
+
   assert (oiN1.has_value());
   assert (oiN1.value() == -1);
-  
+
   assert (oi0.has_value());
   assert (oi0.value() == 0);
 }
@@ -277,38 +297,38 @@ class range
 {
   int min_, max_;
   bool invariant() const { return min_ <= max_; }
-  
+
 public:
-  range(int min, int max) : min_(min), max_(max) 
+  range(int min, int max) : min_(min), max_(max)
   {
     assert (invariant());
     ++objects_created;
-  } 
-  
+  }
+
   range(range const& rhs) : min_(rhs.min_), max_(rhs.max_)
   {
     assert (invariant());
     assert (rhs.invariant());
     ++objects_created;
   }
-  
+
   int min() const
   {
     assert (invariant());
     return min_;
   }
-  
+
   int max() const
   {
     assert (invariant());
     return max_;
   }
-  
+
   friend bool operator==(const range& l, const range& r)
   {
     return l.min_ == r.min_ && l.max_ == r.max_;
   }
-  
+
   ~range()
   {
     assert (invariant());
@@ -333,40 +353,40 @@ struct range2_members
 };
 
 class range2 : private range2_members
-{ 
+{
   bool invariant() const { return min_ <= max_; }
-  
+
 public:
   range2(int min, int max) : range2_members{min, max}
   {
     assert (invariant());
     ++objects_created;
-  } 
-  
+  }
+
   range2(range2 const& rhs) : range2_members(static_cast<range2_members const&>(rhs))
   {
     assert (invariant());
     assert (rhs.invariant());
     ++objects_created;
   }
-  
+
   int min() const
   {
     assert (invariant());
     return min_;
   }
-  
+
   int max() const
   {
     assert (invariant());
     return max_;
   }
-  
+
   friend bool operator==(const range2& l, const range2& r)
   {
     return l.min_ == r.min_ && l.max_ == r.max_;
   }
-  
+
   ~range2()
   {
     assert (invariant());
@@ -378,7 +398,7 @@ struct range2_representation : range2_members
 {
   range2_representation() AK_TOOLKIT_NOEXCEPT : range2_members{0, -1} {};
 };
-  
+
 
 namespace ak_toolkit { namespace markable_ns {
   template<> struct representation_of<range2>
@@ -405,27 +425,27 @@ void test_mark_dual_storage()
     assert ( ot0.has_value());
     assert ( otM.has_value());
     assert(objects_created == 4);
-    
+
     assert (ot0.value() == t0);
     assert (otM.value() == tM);
-    
+
     opt_range otM2 = otM;
     assert (otM.has_value());
     assert (otM.value() == tM);
     assert(objects_created == 5);
-    
+
     ot_ = otM2;
     assert (ot_.has_value());
     assert (ot_.value() == tM);
     assert(objects_created == 6);
     assert(objects_destroyed == 0);
-    
+
     ot_ = {};
     assert (!ot_.has_value());
     assert ( otM.has_value());
     assert(objects_created == 6);
     assert(objects_destroyed == 1);
-    
+
     swap(ot_, otM);
     assert ( ot_.has_value());
     assert (!otM.has_value());
@@ -444,7 +464,7 @@ void test_mark_dual_storage_2() { test_mark_dual_storage<range2, mark_range2>();
 class Date
 {
   int _d;
-  
+
 public:
   bool invariant() const { return _d >= 0 && _d <= 100000; }
   explicit Date (int d) : _d(d) { assert(invariant()); }
@@ -468,20 +488,20 @@ class DateInterval
 {
   Date _first;
   Date _last;
-  
+
 public:
   bool invariant() const { return !(_last < _first); }
   explicit DateInterval(Date const& f, Date const& l) : _first(f), _last(l) { assert (invariant()); ++DateInterval_value_ctor_calls_count; }
   DateInterval(const DateInterval& r) : _first(r._first), _last(r._last) { assert (invariant()); ++DateInterval_copy_ctor_calls_count; }
   DateInterval(DateInterval&& r) : _first(r._first), _last(r._last) { assert (invariant()); ++DateInterval_move_ctor_calls_count; }
   ~DateInterval() { assert (invariant()); ++DateInterval_dtor_calls_count; }
-  
+
   DateInterval& operator=(const DateInterval& r) { assert (invariant()); _first = r._first; _last = r._last; assert (invariant()); return *this; }
   DateInterval& operator=(DateInterval&& r) { assert (invariant()); _first = r._first; _last = r._last; assert (invariant()); return *this; }
-  
+
   Date const& first() const { return _first; }
   Date const& last() const { return _last; }
-  
+
   friend bool operator==(DateInterval const& l, DateInterval const& r) { return l.first() == r.first() && l.last() == r.last(); }
 };
 
@@ -515,7 +535,7 @@ void test_dual_storage_with_tuple_default_and_move_ctor()
     opt_interval o1;
     assert (!o1.has_value());
     assert (count_Interval_value_copy_move_dtror(0, 0, 0, 0));
-    
+
     opt_interval o2 {DateInterval{Date{1}, Date{2}}};
     assert (o2.has_value());
     assert (o2.value().first() == Date{1});
@@ -532,7 +552,7 @@ void test_dual_storage_with_tuple_copy_ctor()
     typedef markable<mark_interval> opt_interval;
     DateInterval i12 {Date{1}, Date{2}};
     assert (count_Interval_value_copy_move_dtror(1, 0, 0, 0));
-    
+
     opt_interval o2 {i12};
     assert (o2.has_value());
     assert (o2.value() == i12);
@@ -548,25 +568,25 @@ void test_dual_storage_with_tuple_init_state_mutation()
     typedef markable<mark_interval> opt_interval;
     DateInterval i12 {Date{1}, Date{2}};
     assert (count_Interval_value_copy_move_dtror(1, 0, 0, 0));
-    
+
     opt_interval o1 {}, o2 {i12};
     assert (count_Interval_value_copy_move_dtror(1, 1, 0, 0));
-    
+
     assert (!o1.has_value());
     assert (o2.has_value());
-    
+
     o1 = opt_interval{i12};
     assert (count_Interval_value_copy_move_dtror(1, 2, 1, 1));
     o2 = {};
     assert (count_Interval_value_copy_move_dtror(1, 2, 1, 2));
-    
+
     assert (o1.has_value());
     assert (o1.value() == i12);
     assert (!o2.has_value());
-    
+
     swap (o1, o2);
     assert (count_Interval_value_copy_move_dtror(1, 2, 2, 3));
-    
+
     assert (!o1.has_value());
     assert (o2.has_value());
     assert (o2.value() == i12);
@@ -581,8 +601,8 @@ void test_dual_storage_with_tuple_init_state_mutation()
 
 int main()
 {
-	
   test_value_ctor();
+  test_assignment();
   test_string_traits();
   test_custom_storage();
   test_bool_storage();
@@ -595,7 +615,7 @@ int main()
 #if defined AK_TOOLKIT_USING_BOOST
   test_optional_as_storage();
 #endif
-  
+
   test_mark_dual_storage_1();
   test_mark_dual_storage_2();
 /*  test_dual_storage_with_tuple_default_and_move_ctor();
